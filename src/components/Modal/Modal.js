25f3462c1979074a321d366b0a558e0aaf62cdea
Ibairb <ibairb1992@gmail.com>
2022-12-12 22:14:46 +0100

    modales y cambios en agenda

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Modal.css";
+
+function Modal({ setOpenModal }) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div className="title">
+          <h1>Fill below fields to create a new user </h1>
+        </div>
+        <div className="body">
+          <form action="POST" className="form">
+            <input type="text" placeholder="Username" className="username"/>
+            <input type="password" placeholder="Password" className="password"/>
+            <input type="email" placeholder="email"/>
+            <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+            className="btn"
+          >
+            Cancel
+          </button>
+          <span></span>
+          <button className="btn" id="continue">Continue</button>
+          </form>
+        </div>
+        <div className="footer">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
