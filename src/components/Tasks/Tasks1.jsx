5df5fd5aec34093ec7f3f831b3d9216bb4f18681
martinjauregui00 <martinjauregui96@hotmail.com>
2022-12-12 13:39:46 +0100

    borrado de instrucciones

--- src/components/Tasks/Tasks1.jsx
+++ src/components/Tasks/Tasks1.jsx
@@ -52,12 +52,7 @@ export default class DemoApp extends React.Component {
     return (
       <div className='demo-app-sidebar'>
         <div className='demo-app-sidebar-section'>
-          <h2>Instructions</h2>
-          <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
+          <h2>Tasks</h2>
         </div>
         <div className='demo-app-sidebar-section'>
           <label>
