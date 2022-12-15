3ba56e1d92b5798297bb73796f5de5eec3387303
martinjauregui00 <martinjauregui96@hotmail.com>
2022-12-15 15:44:32 +0100

    Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import { React, useState } from 'react';
 
 const Login = () => {
-  const [login, setLogin] = useState("false")
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
