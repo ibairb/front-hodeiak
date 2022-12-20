d597adece083c7bfefbc3cc633d2335810496cf0
martinjauregui00 <martinjauregui96@hotmail.com>
2022-12-20 16:45:08 +0100

    css perfil logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
                     <li className='nav-item'>
                         <Link to="/tasks">Tareas</Link>
                     </li>
-                    <li className='login-data'>
+                    <li className='login-data1'>
                         <img src='https://static.wixstatic.com/media/e42ea9_2a5993ef17c94b2b847d264cb801beea~mv2.png/v1/crop/x_0,y_26,w_673,h_621/fill/w_380,h_352,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Profile-PNG-File.png' />
                         <p id='username'>{username}</p>
                     </li>
