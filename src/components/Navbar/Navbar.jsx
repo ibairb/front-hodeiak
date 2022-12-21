9a506720f36d20206b17de29e3edcd11af493064
martinjauregui00 <martinjauregui96@hotmail.com>
2022-12-21 16:40:05 +0100

    Modal cambio datos perfil Y icono perfil

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -41,7 +41,9 @@ const Navbar = () => {
                         <Link to="/tasks">Tareas</Link>
                     </li>
                     <li className='login-data1'>
-                        <img src='https://static.wixstatic.com/media/e42ea9_2a5993ef17c94b2b847d264cb801beea~mv2.png/v1/crop/x_0,y_26,w_673,h_621/fill/w_380,h_352,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Profile-PNG-File.png' />
+                        <span class="material-symbols-outlined">
+                            badge
+                        </span>
                         <p id='username'>{username}</p>
                     </li>
                     <li>
@@ -66,7 +68,9 @@ const Navbar = () => {
                         <Link to='/clients'>Clients</Link>
                     </li>
                     <li className='login-data'>
-                        <img src='https://static.wixstatic.com/media/e42ea9_2a5993ef17c94b2b847d264cb801beea~mv2.png/v1/crop/x_0,y_26,w_673,h_621/fill/w_380,h_352,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Profile-PNG-File.png' />
+                        <span class="material-symbols-outlined">
+                            badge
+                        </span>
                         <p id='username'>{username}</p>
                     </li>
                     <li>
